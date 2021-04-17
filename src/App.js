bbf5d5c17b99239f0fa2a7281fb269e0405a6c66
Chaitanya7666 <56331036+Chaitanya7666@users.noreply.github.com>
2021-04-17 13:21:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import "./styles.css";
+
+var emojis = {
+  "😊": "Smiling",
+  "😔": "Dull Face",
+  "❤️": "Heart",
+  "😑": "No Answer",
+  "😎": "Cool",
+  "😘": "Kiss",
+  "😍": "Heart Eyes"
+};
+
+var emojiList = Object.keys(emojis);
+
+export default function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState("");
+  function clickedEmoji(emoji) {
+    return setEmojiMeaning(emojis[emoji]);
+  }
+  return (
+    <div className="App">
+      <div
+        style={{
+          alignContent: "center",
+          width: "100%",
+          height: "80%",
+          margin: "auto",
+          marginTop: "10rem"
+        }}
+      >
+        <h1 style={{ color: "gray", margin: "2rem" }}>
+          <strong>
+            👾<em>Emoji Interpreter</em>👾
+          </strong>
+        </h1>
+        <input
+          style={{
+            width: "50%",
+            textAlign: "center",
+            fontSize: "1rem",
+            lineHeight: "2rem",
+            marginBottom: "1.2rem"
+          }}
+          placeholder="Enter your smiley here!"
+        ></input>
+        <h2 style={{ marginBottom: "1.1rem" }}>{emojiMeaning}</h2>
+        <h3>These are the emoji's we accept</h3>
+
+        {emojiList.map(function (emoji) {
+          return (
+            <span
+              onClick={() => clickedEmoji(emoji)}
+              style={{ fontSize: "2rem", padding: "1rem", cursor: "pointer" }}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
